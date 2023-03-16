79593c755970a7de6bd15d047a48db751ccb86ec
SirD007 <thedavid2009@gmail.com>
2023-03-16 14:49:46 -0300

    added more categories

---
+++ src/pages/LinhaAltaTensaoData.jsx
@@ -0,0 +1,23 @@
+import h131 from '../assets/h131.png'
+
+export default [
+    {
+      id: "1",
+      imgSrc: `${h131}`,
+      ref: "D1920BK193",
+      SKU: "H131",
+      description: "Fita isolante de PVC elaborada para uso automotivo com ultra aderência. Contém procedimento retardante antichama, excelente flexibilidade, alongamento e resistência às mudanças meteorológicas.",
+      composition: "Poly Vinnyl Chloide",
+      tempClass: "90°C . TEST 135°C x 200hrs",
+      AdesAço: "1.5N/cm(290gf/19mm)",
+      AdesDorso: "1.5N/cm(290gf/19mm)",
+      ResisTraç: "15.8N/cm(3.0kgf/19mm)",
+      Along: "180%",
+      ResistChama: "Auto extinguível",
+      Utility: "Fábrica de chicote, Montadoras e Concessionários Automotivos, Oficinas mecânicas, Loja de som, e uso geral domésticos.",
+      roHS: "",
+      cores: [
+        "bg-black" 
+      ],
+    },
+  ];
