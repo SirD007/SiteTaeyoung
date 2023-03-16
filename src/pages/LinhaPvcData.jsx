8aee5c65e1cc4514252075062bdaa65afd83c13f
SirD007 <thedavid2009@gmail.com>
2023-03-16 14:27:55 -0300

    added product card to LinhaPvc page

--- src/pages/LinhaPvcData.jsx
+++ src/pages/LinhaPvcData.jsx
@@ -1,28 +1,140 @@
+import h131 from '../assets/h131.png'
+import h132 from '../assets/h131.png'
+import s153 from '../assets/h131.png'
+import eg10 from '../assets/h131.png'
+import eg13 from '../assets/h131.png'
+import d193 from '../assets/h131.png'
+
 export default [
     {
       id: "1",
-      name: "Grumpy Cat Poster",
-      description: "Everyone's favorite cat who loves to hate",
-      price: 15
+      imgSrc: `${h131}`,
+      ref: "D1920BK193",
+      SKU: "H131",
+      description: "Fita isolante de PVC elaborada para uso automotivo com ultra aderência. Contém procedimento retardante antichama, excelente flexibilidade, alongamento e resistência às mudanças meteorológicas.",
+      composition: "Poly Vinnyl Chloide",
+      tempClass: "90°C . TEST 135°C x 200hrs",
+      AdesAço: "1.5N/cm(290gf/19mm)",
+      AdesDorso: "1.5N/cm(290gf/19mm)",
+      ResisTraç: "15.8N/cm(3.0kgf/19mm)",
+      Along: "180%",
+      ResistChama: "Auto extinguível",
+      Utility: "Fábrica de chicote, Montadoras e Concessionários Automotivos, Oficinas mecânicas, Loja de som, e uso geral domésticos.",
+      roHS: "",
+      cores: [
+        "bg-black" 
+      ],
     },
     {
       id: "2",
-      name: "Stretch Armstrong",
-      description:
-        "He bends! He stretches! He even ties in knots, but always returns to his original shape!",
-      price: 20
+      imgSrc: `${h132}`,
+      ref: "H1920OR132",
+      SKU: "H132",
+      description: "Fita isolante de PVC elaborada para uso automotivo com ultra aderência. Contém procedimento retardante antichama, excelente flexibilidade, alongamento e resistência às mudanças meteorológicas.",
+      composition: "Poly Vinnyl Chloide",
+      tempClass: "90°C . TEST 135°C x 200hrs",
+      AdesAço: "1.8N/cm(330gf/19mm)",
+      AdesDorso: "1.5N/cm(286gf/19mm)",
+      ResisTraç: "16.8N/cm(3.2kgf/19mm)",
+      Along: "180%",
+      ResistChama: "Auto extinguível",
+      Utility: "Fábrica de chicote, Montadoras e Concessionários Automotivos, Oficinas mecânicas, Loja de som, e uso geral domésticos. Distinção de chicotes por seguimentos (partes especiais como Air-bag) Agrupamento e marcação dos cabos (iluminação, instalaçao de som) Instalação de Telecomunicação entre outros. Bigua, Dic som e outras distribuidoras de Som e Acessórios Automotivos",
+      roHS: "",
+      cores: [
+        "bg-teal-700",
+        "bg-violet-800",
+        "bg-rose-400",
+        "bg-amber-900",
+        "bg-orange-500",
+        "bg-stone-400",
+        "bg-blue-600",
+        "bg-sky-400",
+        "bg-white",
+        "bg-yellow-500",
+        "bg-red-700",
+        "bg-emerald-300"
+      ],
     },
     {
       id: "3",
-      name: "Hungry Hungry Hippos Game",
-      description:
-        "It's a race, it's a chase, hurry up and feed their face! Who will win? No one knows! Feed the hungry hip-ip-pos!",
-      price: 30
+      imgSrc: `${s153}`,
+      ref: "S1920BK153",
+      SKU: "S153",
+      description: "Fita isolante de PVC elaborada para uso automotivo com ultra aderência. Contém procedimento retardante antichama, excelente flexibilidade, alongamento e resistência às mudanças meteorológicas.",
+      composition: "Poly Vinnyl Chloide",
+      tempClass: "90°C . TEST 100°C x 120hrs, 5.0kv/min",
+      AdesAço: "1.8N/cm(341gf/19mm)",
+      AdesDorso: "1.6N/cm(306gf/19mm)",
+      ResisTraç: "34.5N/cm(3.6kgf/19mm)",
+      Along: "220%",
+      ResistChama: "Auto extinguível",
+      Utility: "Fábrica de chicote, Montadoras e Concessionários Automotivos, Oficinas mecânicas, Loja de som, e uso geral domésticos.",
+      roHS: "",
+      cores: [
+        "bg-teal-700",
+        "bg-stone-400",
+        "bg-blue-600",
+        "bg-white",
+        "bg-yellow-500",
+        "bg-red-700",
+        "bg-black"
+      ],
     },
     {
       id: "4",
-      name: "Crossfire board game",
-      description: "You'll get caught up in the crossfire!",
-      price: 35
-    }
+      imgSrc: `${eg10}`,
+      ref: "EG1920BK10",
+      SKU: "EG10",
+      description: "Fita isolante de PVC elaborada para uso automotivo com ultra aderência. Contém procedimento retardante antichama, excelente flexibilidade, alongamento e resistência às mudanças meteorológicas.",
+      composition: "Poly Vinnyl Chloide",
+      tempClass: "100°C . TEST 136°C x 168hrs",
+      AdesAço: "2.5N/cm(480gf/19mm)",
+      AdesDorso: "2.3N/cm(450gf/19mm)",
+      ResisTraç: "22.0N/cm(4.3kgf/19mm)",
+      Along: "230%",
+      ResistChama: "Auto extinguível",
+      Utility: "",
+      roHS: "(cd, Pb, Hg, Cr6+, PBDEs. PBBs free)",
+      cores: [
+        "bg-black"
+      ],
+    },
+    {
+      id: "5",
+      imgSrc: `${eg13}`,
+      ref: "EG1920BK13",
+      SKU: "EG13",
+      description: "Fita isolante de PVC elaborada para uso automotivo com ultra aderência. Contém procedimento retardante antichama, excelente flexibilidade, alongamento e resistência às mudanças meteorológicas.",
+      composition: "Poly Vinnyl Chloide",
+      tempClass: "100°C . TEST 136°C x 168hrs",
+      AdesAço: "2.5N/cm(480gf/19mm)",
+      AdesDorso: "2.3N/cm(450gf/19mm)",
+      ResisTraç: "22.0N/cm(4.3kgf/19mm)",
+      Along: "230%",
+      ResistChama: "Auto extinguível",
+      Utility: "",
+      roHS: "(cd, Pb, Hg, Cr6+, PBDEs. PBBs free)",
+      cores: [
+        "bg-black"
+      ],
+    },
+    {
+      id: "6",
+      imgSrc: `${d193}`,
+      ref: "D1920BK193",
+      SKU: "D193",
+      description: "Fita isolante de PVC elaborada para uso automotivo com ultra aderência. Contém procedimento retardante antichama, excelente flexibilidade, alongamento e resistência às mudanças meteorológicas.",
+      composition: "Poly Vinnyl Chloide",
+      tempClass: "90°C . TEST 100°C x 120hrs, 11,5kv/min",
+      AdesAço: "2.0N/cm(365gf/19mm)",
+      AdesDorso: "1.7N/cm(330gf/19mm)",
+      ResisTraç: "34.2N/cm(6.5kgf/19mm)",
+      Along: "220%",
+      ResistChama: "Auto extinguível",
+      Utility: "Fita profissional de alta aderência, melhor proteção emalta voltagem, utilizada tambémpor empresas de TV à cabos, telecomunicação, náutica e eletricistas profissionais, entre outras aplicações de alta voltagem<br>Atende o mercado telecom para altas tensões.<br>• Instalação de TV a cabo<br>• Operadores e telefonia<br>• Internet",
+      roHS:"",
+      cores: [
+        "bg-black"
+      ],
+    },
   ];
