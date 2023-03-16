d14e6a20931c9e055c72171d1cc59524748e1edd
SirD007 <thedavid2009@gmail.com>
2023-03-16 14:52:07 -0300

    changed product details

--- src/pages/LinhaAltaTensaoData.jsx
+++ src/pages/LinhaAltaTensaoData.jsx
@@ -4,20 +4,20 @@ export default [
     {
       id: "1",
       imgSrc: `${h131}`,
-      ref: "D1920BK193",
-      SKU: "H131",
-      description: "Fita isolante de PVC elaborada para uso automotivo com ultra aderência. Contém procedimento retardante antichama, excelente flexibilidade, alongamento e resistência às mudanças meteorológicas.",
-      composition: "Poly Vinnyl Chloide",
-      tempClass: "90°C . TEST 135°C x 200hrs",
-      AdesAço: "1.5N/cm(290gf/19mm)",
-      AdesDorso: "1.5N/cm(290gf/19mm)",
-      ResisTraç: "15.8N/cm(3.0kgf/19mm)",
-      Along: "180%",
-      ResistChama: "Auto extinguível",
-      Utility: "Fábrica de chicote, Montadoras e Concessionários Automotivos, Oficinas mecânicas, Loja de som, e uso geral domésticos.",
+      ref: "R1905BK800",
+      SKU: "R800",
+      description: "Fita à base de borracha de etileno-propileno com alta conformidade em qualquer tipo de superfície e formulada para fusão instantânea sem a necessidade de aquecimento (Autofusão).desenrolamento fácil, à prova de vibração, redução de som, boa aderência e resistência à tração. Usado para enrolamento de cablagem automáticapoderá ser utilizada para acabamento originados cobos de som na parte traseiro do carro.",
+      composition: "Borracha",
+      tempClass: "90°C . TEST 100°C x 120hrs",
+      AdesAço: "7.9N/cm(1.5kgf/19mm)",
+      AdesDorso: "",
+      ResisTraç: "7.9N/cm(1.5kgf/19mm)",
+      Along: "600%",
+      ResistChama: "",
+      Utility: "Fita profissional de alta aderência, melhor proteção em alta voltagem, utilizada por empresas de TV à cabos, telecomunicação, náutica e eletricistas profissionais, entre outras aplicações de alta voltagem, Vedação contra a umidade, para proteger a isolação dos cabos de potência. Proteção contra a penetração de umidade pelas pontas dos cabos de potência. Esta fita deve sempre ser coberta com uma camada da Fita Isolante D193 para proteção contra intempéries e ação do tempo. Atende o mercado telecom para altas tensões. • Instalação de TV a cabo • Operadores e telefonia e Internet cabo",
       roHS: "",
       cores: [
-        "bg-black" 
+        "bg-white" 
       ],
     },
   ];
