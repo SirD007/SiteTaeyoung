7a7e8aa84a2143337abeaa49cfa7b03045c0b715
SirD007 <thedavid2009@gmail.com>
2023-03-17 10:23:54 -0300

    added and linked product detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -10,22 +10,57 @@ function ProductDetail() {
     return (
         <div>
             <HeaderMenu/>
-            <div key={productId} className="h-screen w-screen bg-gray-900 flex relative justify-center items-center">
-                <div>
-                    <Link className="text-white font-semibold text-xl absolute left-[30px] top-28 bg-gray-700 hover:bg-gray-800 p-2 rounded transition-all" to="/products">← Voltar</Link>
-                </div>
-                <div className="flex gap-8">                                    
-                    <div className="p-5 bg-white rounded-xl h-[400px] w-[350px] flex hover:scale-105 transition-all shadow-xl relative">
-                        <img className="" src={thisProduct.imgSrc} alt="" />
-                        <div className="bg-primaryBlue rounded-xl h-[150px] w-[130px] absolute top-[245px] left-[-60px] shadow-lg flex justify-center items-center" ><img src={thisProduct.imgSrc} alt="" /></div>
-                    </div>
-                    <div className="shadow-xl w-[350px] h-[500px] rounded-xl bg-white">
-                        <div>
-                            <h1>{thisProduct.SKU}</h1>
-                            <span>Ref: {thisProduct.ref}</span>
+            <div key={productId} className="px-[10%] h-screen w-screen bg-gray-900 flex relative justify-center items-center">
+                <div className="bg-slate-800 w-[90%] h-28 shadow-2xl rounded-lg absolute bottom-4 z-50 flex justify-between items-center">
+                    <Link className="text-red-700 font-semibold text-lg bg-red-300 hover:bg-red-400 p-2 rounded transition-all flex items-center justify-center w-28 h-full" to="/products">← Voltar</Link>
+                    <div>
+                        <span className="text-white">Cores</span>
+                        <div className="flex gap-2">
+                            {thisProduct.cores.map((cores, index) => {
+                                return (
+                                    <div key={index} className={`${cores} w-5 h-5 rounded-[50%] border-2`}></div>
+                                )
+                            })}
                         </div>
-                        <span>{thisProduct.composition}</span>
-                        <span>{thisProduct.description}</span>
+                    </div>
+                    <div className="h-full w-1/2 flex flex-col justify-center">
+                        <span className="text-white">Utilizado em</span>
+                        <span className="text-white text-sm">{thisProduct.Utility}</span>
+                    </div>
+                </div>
+                <div className="w-screen h-screen grid grid-cols-3 justify-center items-center relative">
+                    <div className="p-10">
+                        <img src={thisProduct.imgSrc} alt="" />
+                    </div>
+                    <div className="flex flex-col p-10">
+                        <div>
+                            <h1 className="text-zinc-200 font-black text-8xl select-none">{thisProduct.SKU}</h1>
+                            <span className="text-white font-light text-lg select-none">Ref: {thisProduct.ref}</span>
+                        </div>
+                        <span className="text-zinc-400">{thisProduct.description}</span>
+                    </div>
+                    <div className="rounded-lg overflow-hidden shadow-lg flex flex-col w-1/2">
+                        <div className="flex flex-col bg-gray-700 p-5">
+                            <span className="text-xl font-black text-white">{thisProduct.SKU}</span>
+                            <span className="text-sm text-zinc-400">{thisProduct.composition}</span>
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="flex flex-col bg-slate-800 p-3">
+                                <span className="text-xs uppercase text-gray-400 font-bold">Adesão ao Aço</span><span className="text-white">{thisProduct.AdesAço}</span>
+                            </div>
+                            <div className="flex flex-col bg-slate-700 p-3">
+                                <span className="text-xs uppercase text-gray-400 font-bold">Adesão ao Dorso</span><span className="text-white">{thisProduct.AdesDorso}</span>
+                            </div>
+                            <div className="flex flex-col bg-slate-800 p-3">
+                                <span className="text-xs uppercase text-gray-400 font-bold">Resistência a Tração</span><span className="text-white">{thisProduct.ResisTraç}</span>
+                            </div>
+                            <div className="flex flex-col bg-slate-700 p-3">
+                                <span className="text-xs uppercase text-gray-400 font-bold">Alongamento</span><span className="text-white">{thisProduct.Along}</span>
+                            </div>
+                            <div className="flex flex-col bg-slate-800 p-3">
+                                <span className="text-xs uppercase text-gray-400 font-bold">Resistência a chama</span><span className="text-white">{thisProduct.ResistChama}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
